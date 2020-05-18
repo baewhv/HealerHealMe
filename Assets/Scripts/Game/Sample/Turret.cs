360a73648f6f8e9593263a0778518c2a8b376d31
BaeJeongMin <baewhv@naver.com>
2020-05-18 15:47:59 +0900

    [ADD] : 타워 샘플 추가.

---
+++ Assets/Scripts/Game/Sample/Turret.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace sample
+{
+    public class Turret : MonoBehaviour
+    {
+        Transform target;
+
+        public float range = 15f;
+
+        public string enemyTag = "Enemy";
+
+        public Transform partToRotate;
+        public float turnSpeed = 5f;
+
+        void Start()
+        {
+            InvokeRepeating("UpdateTarget", 0f, 0.5f);
+        }
+
+        void UpdateTarget()
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+            float shortestDistance = Mathf.Infinity;
+            GameObject nearestEnemy = null;
+
+            foreach (GameObject enemy in enemies)
+            {
+                float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+                if(distanceToEnemy < shortestDistance)
+                {
+                    shortestDistance = distanceToEnemy;
+                    nearestEnemy = enemy;
+                }
+            }
+            if(nearestEnemy != null && shortestDistance <= range)
+            {
+                target = nearestEnemy.transform;
+            }
+        }
+
+        void Update()
+        {
+            if (target == null)
+            {
+                return;
+            }
+
+
+            //target lock on
+
+            Vector3 dir = target.position - transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+            Debug.Log(rotation);
+            partToRotate.rotation = Quaternion.Euler(0f, 0f, rotation.x);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, range);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Sample/WaveSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace sample
+{
+    public class WaveSpawner : SingletonMonoBehaviour<WaveSpawner>
+    {
+        public Transform enemyPrefab;
+
+        public Transform SpawnPoint;
+
+        public float timerBetweenWaves = 5.0f;
+        private float countDown = 2.0f;
+
+        public Text waveCountdownText;
+
+        private int waveIndex = 1;
+
+        private void Update()
+        {
+            if (countDown <= 0f)
+            {
+                StartCoroutine(SpawnWave());
+                countDown = timerBetweenWaves;
+            }
+
+            countDown -= Time.deltaTime;
+
+            //waveCountdownText.text = Mathf.Floor(countDown).ToString();
+            //Floor 가장 큰 int값을 내보냄. 
+            waveCountdownText.text = Mathf.Round(countDown).ToString();
+            
+        }
+
+        IEnumerator SpawnWave()
+        {
+            waveIndex++;
+
+            for (int i =0; i < waveIndex; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(1f);
+            }
+            Debug.Log("Wave Incomming!");
+            
+        }
+
+        void SpawnEnemy()
+        {
+            Instantiate(enemyPrefab, SpawnPoint.position, SpawnPoint.rotation);
+        }
+
+    }
+}
